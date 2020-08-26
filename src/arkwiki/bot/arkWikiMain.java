e10d8b758268b1bc41d031a8af9bfdb709213c63
Orisland <756176532@qq.com>
2020-08-26 22:50:02 +0800

    bot 方舟wiki新功能添加

---
+++ src/arkwiki/bot/arkWikiMain.java
@@ -0,0 +1,209 @@
+package arkwiki.bot;
+
+import com.alibaba.fastjson.JSONArray;
+import com.alibaba.fastjson.JSONObject;
+import arkwiki.doc;
+
+import java.util.concurrent.TimeUnit;
+
+//涩图启动类
+
+/**
+ * 主要类
+ * @author zhaoqk
+ *
+ * 2020年8月10日 下午5:07:37
+ */
+public class arkWikiMain {
+	public static long selfQQ=0;
+	public static long fromGroup=0;
+	public static long fromQQ=0;
+
+	/**
+	 * 程序简介
+	 * 1.如果我需要的事件下面没有怎么办？
+	 * 答：作者是采集了一些常用的如果你需要其他事件 比如 好友更改签名
+	 * 	     那你就启动项目 用机器人号的好友改一下签名 就会收到改签名的消息 看看msgType是对多少然后下面加个判断就行了。
+	 * 2.如果我需要的API操作Core中没有怎么办？
+	 * 答：如果你具备易语言开发能力请下载TcpAPI的源码然后按照格式增加一个API然后在Core中也增加一个对应的就可以了
+	 *    如果你不具备易语言开发能力请加群742830386向我反映你需要的API 我会在更新时一块加上
+	 * 3.你的插件不收费还开源为了什么？
+	 * 答：这个东西对于我来说很简单，本来打算做一个自己玩玩然后越做越完善于是就开源大家一起玩
+	 *    开源是因为我的维护时间不多你需要的可能我无法帮你完善，所以我开个头 你们 尽情发挥^_^
+	 */
+
+	static ChatClient clientTest;
+
+	public static void main(String[] args) {
+		clientTest = new ChatClient("127.0.0.1", 8404);
+		clientTest.start();
+	}
+
+	/**
+	 * 收到好友消息与事件
+	 * @param data
+	 */
+	public static void receivePrivateMessages(String data){
+
+		System.out.println("[收到好友消息]" + data);
+		try{
+			JSONObject json = JSONObject.parseObject(data);
+			selfQQ = json.getInteger("selfQQ");//框架QQ
+			fromQQ = json.getInteger("fromQQ");//对方QQ
+			long random = json.getInteger("random");//撤回消息用
+			long req = json.getInteger("req");//撤回消息用
+			String msg = json.getString("msg");//消息内容
+			long msgType = json.getInteger("msgType");//消息类型
+			//long msgType2 = json.getInteger("msgType2");//消息类型
+			if(msgType == 141){//公众号消息
+
+			}else if(msgType == 87){//好友邀请入群
+
+			}else if(msgType == 528){//申请加为好友
+
+			}else if(msgType == 529){//好友文件消息
+
+			}else if(msgType == 208){//好友语音消息
+				req = 0L;
+				random = 0L;
+				Core.sendPrivateMessages(selfQQ, fromQQ, msg, random, req);
+			}else if(msgType == 87){//好友邀请入群
+
+			}else if(msgType == 166){//普通好友消息
+				if (msg.equals("test")) {
+					Core.sendPrivateMessages(selfQQ,fromQQ,"测试有效",random,req);
+				}
+				else {
+					msg += "~";
+					Core.sendPrivateMessages(selfQQ, fromQQ, msg, random, req);
+				}
+			}
+		}catch (Exception e) {
+			System.out.println("[好友数据异常]");
+		}
+
+		//红包发送成功  "msgType":141,"msgType2":134,"msgTempType":129
+		//87	好友邀请入群
+		//528	申请加为好友
+		//166	收到好友红包
+	}
+	/**
+	 * 收到群聊消息
+	 * @param data
+	 */
+	public static void receiveGroupMessages(String data){
+		System.out.println("[收到群聊消息]" + data);
+
+		String msg;
+		try{
+			JSONObject json = JSONObject.parseObject(data);
+			selfQQ = json.getInteger("selfQQ");//框架QQ
+			fromGroup = json.getInteger("fromGroup");//群号
+			fromQQ = json.getInteger("fromQQ");//对方QQ
+			msg = json.getString("msg");//消息内容
+
+			//这里我写了一些常用指令
+			if (msg.indexOf("方舟") == 0){
+				String base64 = "";
+				System.out.println("加载");
+				String[] msgSplit = msg.split( " ");
+				String juse = msgSplit[1];
+				if (msgSplit.length == 3 && msgSplit[2].equals("1")){
+						 base64 = doc.docbase64(juse,1);
+				}
+				else {
+					base64 = doc.docbase64(juse,0);
+				}
+				String pic = "[pic:"+base64+"]";
+				Core.sendGroupMessagesPicText(selfQQ,fromGroup,pic,0);
+			}
+
+		}catch (Exception e){
+			System.out.println("[群聊数据异常]");
+			e.printStackTrace();
+		}
+		//134	上传群文件
+
+	}
+
+	/**
+	 * 收到事件消息
+	 * @param data
+	 */
+	public static void receiveEventMessages(String data){
+		System.out.println("[收到事件消息]" + data);
+		try{
+			JSONObject json = JSONObject.parseObject(data);
+			long selfQQ = json.getInteger("selfQQ");//框架QQ
+			long fromGroup = json.getInteger("fromGroup");//群号
+			int msgType = json.getInteger("msgType");//类型
+			long triggerQQ = json.getInteger("triggerQQ");//对方QQ
+			//String triggerQQName = json.getString("triggerQQName");//对方昵称
+			long seq = json.getLongValue("seq");//操作用
+
+			//32表示QQ上线
+			//17表示好友更改昵称
+			//25表示邀请加入了群聊
+			if(msgType == 3){//群验证事件 申请入群
+				Core.handleGroupEvent(selfQQ, fromGroup, triggerQQ, seq, 11, 3);
+			}else if(msgType == 20){//申请加为好友
+				Core.handlePrivateEvent(selfQQ, triggerQQ, seq, 1);
+			}else if(msgType == 19){//有新好友
+				Core.sendPrivateMessages(selfQQ, triggerQQ, "哈喽", 0, 0);
+			}else if(msgType == 23){
+				Core.callpPraise(selfQQ,triggerQQ,10);
+			}
+		}catch (Exception e) {
+			System.out.println("[事件数据异常]");
+		}
+
+	}
+
+	/**
+	 * 收到查询返回
+	 * @param data
+	 */
+	public static void selectResult(String data){
+		System.out.println("[收到查询返回]" + data);
+		JSONObject json = JSONObject.parseObject(data);
+		//long selfQQ = json.getInteger("selfQQ");//框架QQ
+		int msgType = json.getInteger("msgType");//类型
+		if(msgType == 107){
+			JSONArray list = json.getJSONArray("list");
+			System.out.println("[好友列表返回]" + list.size());
+		}else if(msgType == 109){
+			JSONObject userInfo = json.getJSONObject("info");
+			System.out.println("[好友信息返回]" + JSONObject.toJSONString(userInfo));
+		}else if(msgType == 212){
+			JSONArray list = json.getJSONArray("list");
+			System.out.println("[群聊列表返回]" + list.size());
+		}else if(msgType == 213){
+			JSONObject groupInfo = json.getJSONObject("info");
+			System.out.println("[群聊信息返回]" + JSONObject.toJSONString(groupInfo));
+		}else if(msgType == 302){
+			String path = json.getString("path");
+			System.out.println("[插件路径返回]" + path);
+		}else if(msgType == 303){
+			String picCode = json.getString("picCode");
+			System.out.println("[上传图片返回]" + picCode);
+		}else if(msgType == 304){
+			String audioCode = json.getString("audioCode");
+			System.out.println("[上传语音返回]" + audioCode);
+			//处理异步
+			long t = json.getLongValue("t");
+			JSONObject jsonRes = StringUtils.queue.get(t);
+			if(jsonRes.getBooleanValue("sync")){//同步发送
+				long selfQQ = jsonRes.getLongValue("selfQQ");
+				long fromQQ = jsonRes.getLongValue("fromQQ");
+				if(jsonRes.getIntValue("msgType") == 1){//好友
+					audioCode = audioCode.replace("voice_codec=0", "voice_codec=1");
+					Core.sendPrivateMessages(selfQQ, fromQQ, audioCode, 0, 0);
+				}else{//群聊
+
+				}
+			}
+
+
+		}
+	}
+}
---
+++ src/arkwiki/doc.java
@@ -0,0 +1,96 @@
+package arkwiki;
+
+import arkwiki.bot.Core;
+import com.sun.java.swing.plaf.windows.resources.windows;
+import org.openqa.selenium.*;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.logging.LogEntries;
+import org.openqa.selenium.logging.LogEntry;
+import org.openqa.selenium.logging.LogType;
+import arkwiki.bot.arkWikiMain;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
+public class doc {
+
+    public static String docbase64(String juse, int i) throws InterruptedException {
+        System.setProperty("webdriver.chrome.driver", "C:\\Program Files (x86)\\Google\\Chrome\\Application\\chromedriver.exe");
+        WebDriver driver;
+        ChromeOptions chromeOptions=new ChromeOptions();
+        chromeOptions.addArguments("-headless");
+        driver = new ChromeDriver(chromeOptions);
+        driver.manage().timeouts().implicitlyWait(2, TimeUnit.SECONDS);
+        String url1 = "http://prts.wiki/w/";
+        String url2 = "#.E5.90.8E.E5.8B.A4.E6.8A.80.E8.83.BD";
+        String url;
+        if (i == 1){
+            url = url1 + juse;
+            driver.manage().window().setSize(new Dimension(900,6700));
+            Core.sendGroupMessages(arkWikiMain.selfQQ,arkWikiMain.fromGroup,"长图模式~",0);
+        }
+        else {
+            url = url1+juse+url2;
+            driver.manage().window().setSize(new Dimension(900,1200));
+            Core.sendGroupMessages(arkWikiMain.selfQQ,arkWikiMain.fromGroup,"区域模式~",0);
+        }
+        System.out.println(url);
+        System.out.println("加载页面");
+        driver.manage().timeouts().pageLoadTimeout(10,TimeUnit.SECONDS);
+        driver.get(url);
+        //((JavascriptExecutor)driver).executeScript("document.body.style.zoom='0.9'");
+        ((JavascriptExecutor)driver).executeScript("window.scrollBy(100,0)");
+        System.out.println("chrome页面加载");
+        Thread.sleep(3000);
+        String pic = ((TakesScreenshot)driver).getScreenshotAs(OutputType.BASE64);
+        System.out.println("转化完毕");
+        driver.close();
+
+        return pic;
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+        System.setProperty("webdriver.chrome.driver", "C:\\Program Files (x86)\\Google\\Chrome\\Application\\chromedriver.exe");
+        WebDriver driver;
+        driver = new ChromeDriver();
+        driver.manage().timeouts().implicitlyWait(2, TimeUnit.SECONDS);
+        driver.manage().window().setSize(new Dimension(900,2000));
+
+//        driver.get("http://prts.wiki/w/巫恋#.E7.B2.BE.E8.8B.B1.E5.8C.96.E6.9D.90.E6.96.99");
+        driver.get("http://prts.wiki/w/巫恋");
+//        ((JavascriptExecutor)driver).executeScript("window.scrollTo(0,4550)");
+        ((JavascriptExecutor)driver).executeScript("document.body.style.zoom='0.9'");
+        ((JavascriptExecutor)driver).executeScript("window.scrollBy(100,4020)");
+//        System.out.println(((JavascriptExecutor)driver).executeScript("window.getScrollX"));
+//        System.out.println(((JavascriptExecutor)driver).executeScript("window.getScrollY"));;
+        Thread.sleep(3000);
+        File pic = ((TakesScreenshot)driver).getScreenshotAs(OutputType.FILE);
+        try
+        {
+            org.apache.commons.io.FileUtils.copyFile(pic, new File("E:\\IdeaLib\\xiaolizi\\123.png"));  //使用copyFile()方法保存获取到的截图文件
+        } catch (IOException e)
+        {
+            e.printStackTrace();
+        }
+//        System.out.println(driver.findElements(By.tagName("table")).size());
+//        List<WebElement> list = driver.findElements(By.tagName("table"));
+//        for (WebElement webElement : list){
+//            System.out.println(webElement.getText());
+//        }
+        //System.out.println(driver.getPageSource());
+        driver.manage().deleteAllCookies();
+        driver.close();
+
+
+//        try {
+//            Thread.sleep(3000);
+//        }catch (Exception e){
+//            e.printStackTrace();
+//        }
+//        driver.close();
+    }
+}
